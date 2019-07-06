5669edc9e59e3a0f6359220c1fedb17f56c0e4fd
Lopez Damian <dennyslopez2013@gmail.com>
2019-07-06 12:37:38 -0500

    Turno

---
+++ src/turnos/Turno.java
@@ -0,0 +1,47 @@
+package turnos;
+
+import java.util.List;
+
+/**
+ *
+ * @author lopdam
+ */
+public class Turno {
+
+    private static int turnos = 0;
+
+    //La prioridad de los turnos se establese dependieno la condicion del migrante.
+    /*
+    Capacidades Especiales=1
+    Tercera Edad=2
+    Atencion Normal=3
+     */
+    private String tipo;
+    private int prioridad;
+
+    //Combinacion de letras y numero
+    private String id;
+    
+    private static final String[] letra={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","V","W","X","Y","Z"};
+    
+    
+    public Turno(int prioridad) {
+        turnos++;
+        this.prioridad = prioridad;
+
+        switch (prioridad) {
+            case 1:
+                this.tipo = "Capacidades Especiales";
+                break;
+            case 2:
+                this.tipo = "Tercera Edad";
+                break;
+            case 3:
+                this.tipo = "Atencion Normal";
+                break;
+
+        }
+
+    }
+
+}
