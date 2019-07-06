7649361691fa302d76b1fc3d9ac474a82908abd7
Lopez Damian <dennyslopez2013@gmail.com>
2019-07-06 17:28:16 -0500

    Escribir migraciones

--- src/Migracion/ReaderWriter.java
+++ src/Migracion/ReaderWriter.java
@@ -6,11 +6,14 @@
 package Migracion;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import pantallas.PantallaBusqueda;
@@ -21,21 +24,20 @@ import pantallas.Pantallas;
  * @author Odalys
  */
 public class ReaderWriter {
-
+    
     public static void leerRegistrosM() {
-
+        
         try (BufferedReader in = new BufferedReader(new FileReader("./src/images/migraciones.txt"))) {
-            String line="";
-
+            String line = "";
+            
             while ((line = in.readLine()) != null) {
                 String[] migraciones = line.split(",");
                 Migrante mig = new Migrante(migraciones[6], migraciones[7], migraciones[8], new SimpleDateFormat("dd/MM/yyyy").parse(migraciones[9]), Integer.valueOf(migraciones[10]));
                 Migracion migracion = new Migracion(migraciones[0], migraciones[1], new SimpleDateFormat("dd/MM/yyyy").parse(migraciones[2]), new SimpleDateFormat("dd/MM/yyyy").parse(migraciones[3]), migraciones[4], migraciones[5], mig);
                 Migracion.agregarRegistro(migracion);
                 
-
             }
-
+            
         } catch (FileNotFoundException ex) {
             Logger.getLogger(Pantallas.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
@@ -43,7 +45,23 @@ public class ReaderWriter {
         } catch (ParseException pe) {
             System.out.println(pe.toString());
         }
-
+        
     }
 
+    public static void writerRegistrosM() {
+        Iterator<Migracion> it = Migracion.getRegistroMigratorios().iterator();
+        try (FileWriter out = new FileWriter("./src/images/migraciones.txt");
+                BufferedWriter bw = new BufferedWriter(out)) {
+            
+            while (it.hasNext()) {
+                bw.write(it.next().toString());
+            }
+        } catch (FileNotFoundException f) {
+            System.out.println(f.toString());
+        } catch (IOException io) {
+            System.out.println(io.toString());
+        }
+        
+    }
+    
 }
